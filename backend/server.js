c30058f83966502f4c3aa7f256878edb28df6c5d
royaldonpie <irakizapatrick@yahoo.com>
2026-03-03 10:45:46 +0100

    Fix Express 5 routing issue: Replace '*' with /.*/

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html')));
+    app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
